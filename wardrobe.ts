c661561134dc4bded608d72c232ae232f03c3e56
Atiq Israk <atiqisrak@gmail.com>
2025-10-07 13:01:51 +0600

    updated waredrobe

--- wardrobe.ts
+++ wardrobe.ts
@@ -18,5 +18,65 @@ export const defaultWardrobe: WardrobeItem[] = [
     id: 'sweat-shirt',
     name: 'Sweat Shirt',
     url: `${MEDIA_URL}sweat1.jpg`,
+  },
+  {
+    id: 'dress-blue',
+    name: 'Dress Blue',
+    url: `${MEDIA_URL}f1.png`,
+  },
+  {
+    id: 'dress-green',
+    name: 'Dress Green',
+    url: `${MEDIA_URL}f2.png`,
+  },
+  {
+    id: 'dress-red',
+    name: 'Dress Red',
+    url: `${MEDIA_URL}f5.png`,
+  },
+  {
+    id: 'dress-orange',
+    name: 'Dress Orange',
+    url: `${MEDIA_URL}f3.png`,
+  },
+  {
+    id: 'dress-teal',
+    name: 'Dress Teal',
+    url: `${MEDIA_URL}f4.png`,
+  },
+  {
+    id: 'men-drop',
+    name: 'Men Drop Shoulder',
+    url: `${MEDIA_URL}m1.png`,
+  },
+  {
+    id: 'men-drop-shoulder',
+    name: 'Men Drop Shoulder',
+    url: `${MEDIA_URL}m2.png`,
+  },
+  {
+    id: 'men-formal-red',
+    name: 'Men Formal Red',
+    url: `${MEDIA_URL}m3.png`,
+  },
+  {
+    id: 'men-formal-blue',
+    name: 'Men Formal Blue',
+    url: `${MEDIA_URL}m4.png`,
+  },
+  {
+    id: 'men-formal-ash',
+    name: 'Men Formal Ash',
+    url: `${MEDIA_URL}m5.png`,
+  },
+  {
+    id: 'women-saree-red',
+    name: 'Women Saree Red',
+    url: `${MEDIA_URL}s1.png`,
+  },
+  {
+    id: 'women-saree-yellow',
+    name: 'Women Saree Yellow',
+    url: `${MEDIA_URL}s2.png`,
   }
 ];
